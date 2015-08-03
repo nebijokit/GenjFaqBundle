0e54c2e7f27f1515460d6591954e72c8f1578e08
vytautas <vytautas@eface.lt>
2015-08-03 17:27:13 +0300

    add use statement for doctrine ArrayCollection class

--- src/Genj/FaqBundle/Entity/Question.php
+++ src/Genj/FaqBundle/Entity/Question.php
@@ -4,6 +4,7 @@ namespace Genj\FaqBundle\Entity;
 
 use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Class Question
